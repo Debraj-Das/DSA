df5d0f9485303b2533912d6d79a448a94b0855aa
Debraj-Das <debrajdas.gayatri@gmail.com>
2023-03-06 02:28:06 +0530

    Arrenge Some Files to Different folder

--- try.cpp
+++ try.cpp
@@ -324,8 +324,77 @@
 	পরে তার পাঠ দেয়।
 */
 
+// #include <bits/stdc++.h>
+// using namespace std;
+// void solve();
+
+// int32_t main()
+// {
+// 	ios_base::sync_with_stdio(0);
+// 	cin.tie(0);
+// 	cout.tie(0);
+
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// #endif
+
+// 	clock_t z = clock();
+
+// 	int t = 1;
+// 	//	cin >> t;
+// 	while (t--)
+// 		solve(); // solve the problem inside of drive function for test cases.
+
+// 	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+// 	return 0;
+// }
+
+// void solve()
+// {
+// 	int anw = 1;
+// 	for (int i = 0; i < 23; i++)
+// 	{
+// 		cout << (i + 1) << ' ';
+// 		anw = ((anw % 153) * 147) % 153;
+// 	}
+// 	cout << endl
+// 		 << anw << endl;
+
+// 	return;
+// }
+
+/*
+	অভিজ্ঞতা একটি কঠিন শিক্ষক,
+	সে প্রথমে তোমার পরীক্ষা নেয় এবং
+	পরে তার পাঠ দেয়।
+*/
+
+/*
+	অভিজ্ঞতা একটি কঠিন শিক্ষক,
+	সে প্রথমে তোমার পরীক্ষা নেয় এবং
+	পরে তার পাঠ দেয়।
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
+
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char *name, Arg1 &&arg1)
+{
+	cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&...args)
+{
+	const char *comma = strchr(names + 1, ',');
+	cout.write(names, comma - names) << " : " << arg1 << " | ";
+	__f(comma + 1, args...);
+}
+
+#define endl '\n'
 void solve();
 
 int32_t main()
@@ -342,9 +411,9 @@ int32_t main()
 	clock_t z = clock();
 
 	int t = 1;
-	//	cin >> t;
+		// cin >> t;
 	while (t--)
-		solve(); // solve the problem inside of drive function for test cases.
+		solve();
 
 	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
 
@@ -353,14 +422,14 @@ int32_t main()
 
 void solve()
 {
-	int anw = 1;
-	for (int i = 0; i < 23; i++)
-	{
-		cout << (i + 1) << ' ';
-		anw = ((anw % 153) * 147) % 153;
-	}
-	cout << endl
-		 << anw << endl;
+
+	int number = 0b011; //binary form, This type of assignment called at declearted time called literals
+	cout << number<<endl;
+	number = 012;	// Octa number
+	cout << number<<endl;
+	number = 0x12 ;	// Hex decimal
+	cout << number<<endl;
+
 
 	return;
 }
