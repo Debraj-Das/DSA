a4c2ae19980d5eac763b7808a2696dbe5413cc1b
Debraj-Das <debrajdas.gayatri@gmail.com>
2023-04-23 15:56:41 +0530

    Dynamic programming template

--- Dynamic_programming_Template.cpp
+++ Dynamic_programming_Template.cpp
@@ -1,3 +1,7 @@
+/*
+    * In this file we will see the template for the dynamic programming problem
+    so I store mother problem in the dynamic programming problem in this file
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -58,3 +62,69 @@ void solve()
 
     return ;
 }
+
+/*
+    Dynamic programming is a method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solutions using a memory-based data structure.
+    Dynamic programming is both a mathematical optimization method and a computer programming method.
+    In both contexts it refers to simplifying a complicated problem by breaking it down into simpler subproblems in a recursive manner.
+    While some decision problems cannot be taken apart this way, decisions that span several points in time do often break apart recursively.
+    Likewise, in computer science, if a problem can be solved optimally by breaking it into subproblems and then recursively finding the optimal solutions to the subproblems, then it is said to have optimal substructure.
+    A dynamic programming algorithm examines each subproblem only once, typically in a top-down fashion, and so has exponential time complexity.
+    The saving of work by the memoization allows the evaluation of each recursive call in constant time, so that the complete algorithm runs in time proportional to the number of items multiplied by the knapsack capacity.
+
+    In Dynamic programming we have two approach
+    1. Top down approach or memorised approach
+    2. Bottom up approach or tabulation approach
+
+*    There some mother problem in dynamic programming
+    1. 0-1 knapsack problem
+    2. Unbounded knapsack problem
+    3. Longest common subsequence
+    4. Longest increasing subsequence
+    5. Edit distance
+    6. Coin change problem
+    7. Matrix chain multiplication
+    8. Rod cutting problem
+    9. Partition problem
+    10. Subset sum problem
+    11. Word break problem
+    12. Palindrome partitioning problem
+    13. Egg dropping problem
+    14. Boolean parenthesization problem
+    15. 0-1 knapsack problem with duplicate items
+    16. Minimum sum partition problem
+    17. Count number of ways to cover a distance
+    18. Minimum number of jumps to reach end
+    19. Longest path in matrix
+    20. Count number of ways to reach a given score in a game
+    21. Maximum sum increasing subsequence
+    22. Maximum length chain of pairs
+    23. Maximum sum rectangle in a 2D matrix
+    24. Maximum size square sub-matrix with all 1s
+    25. Maximum sum path in two arrays
+    26. Maximum length of pair chain
+    27. Maximum sum of i*arr[i] among all rotations of a given array
+    28. Maximum profit by buying and selling a share at most twice
+    29. Maximum profit by buying and selling a share at most k times
+    30. Maximum profit by buying and selling a share any number of times
+    31. Maximum profit by buying and selling a share at most once with transaction fee
+    32. Maximum profit by buying and selling a share at most once with cooldown
+    33. Maximum profit by buying and selling a share at most once with k transaction
+    34. Maximum profit by buying and selling a share at most once with k transaction and cooldown
+    35. Maximum profit by buying and selling a share at most once with k transaction and transaction fee
+    36. Maximum profit by buying and selling a share at most once with k transaction and transaction fee and cooldown
+    37. Maximum profit by buying and selling a share at most once with k transaction and transaction fee and cooldown and transaction fee
+    38. Maximum profit by buying and selling a share at most once with k transaction and transaction fee and cooldown and transaction fee and cooldown
+    39. Maximum profit by buying and selling a share at most once with k transaction and transaction fee and cooldown and transaction fee and cooldown and transaction fee
+    40. Maximum profit by buying and selling a share at most once with k transaction and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown
+    41. Maximum profit by buying and selling a share at most once with k transaction and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown and transaction fee
+    42. Maximum profit by buying and selling a share at most once with k transaction and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown
+    43. Maximum profit by buying and selling a share at most once with k transaction and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown and transaction fee
+    44. Maximum profit by buying and selling a share at most once with k transaction and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown
+    45. Maximum profit by buying and selling a share at most once with k transaction and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown and transaction fee and cooldown and transaction fee
+    
+
+
+
+
+*/
--- Rough.cpp
+++ Rough.cpp
@@ -1,41 +1,31 @@
 
-/*
-    অভিজ্ঞতা একটি কঠিন শিক্ষক,
-    সে প্রথমে তোমার পরীক্ষা নেয় এবং
-    পরে তার পাঠ দেয়।
-*/
-
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-#define int long long
+#define int long long int
 #define el '\n'
-#define f(i,a,b) for(int i = a; i < b; i++)
-#define fr(i,a,b) for(int i = a; i >= b; i--)
-#define vi vector<int>
-void solve() ;
+void solve();
 
 int32_t main()
 {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
 #ifndef ONLINE_JUDGE
-    freopen("input.txt",  "r",  stdin);
+    freopen("input.txt", "r", stdin);
 #endif
-
     int t = 1;
-	cin >> t;
-    while (t--) solve();
-
+    // cin>>t ;
+    while (t--)
+        solve();
     return 0;
 }
 
 void solve()
 {
-    // take two input and sum them
-    int a, b;  cin >> a >> b;
-    cout << a + b << el;
-    
-    return ;
+    int x , y ;
+    cin >> x >> y ;
+    cout<< x+y << el ; 
+    return;
 }
 
 // //{ Driver Code Starts
