40d7fecad2d7d573ddccadeb3ccf5a0bdf60ee04
Debraj-Das <debrajdas.gayatri@gmail.com>
2023-05-23 02:03:54 +0530

    Automated all final changes to the repository using python script                 This is the beautiful script I have ever written for automation

--- CPP_files/Unique_number.cpp
+++ CPP_files/Unique_number.cpp
@@ -1,29 +1,58 @@
+/*
+    অভিজ্ঞতা একটি কঠিন শিক্ষক,
+    সে প্রথমে তোমার পরীক্ষা নেয় এবং
+    পরে তার পাঠ দেয়।
+*/
+
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std ;
 
-int unique_number(vector<int> arr)
-{
-    int unq = 0 ;
-    for(int x:arr)
-    unq ^= x ;
-    return unq ;
-}
+#define int long long
+#define el '\n'
+#define f(i,a,b) for(int i = a; i < b; i++)
+#define fr(i,a,b) for(int i = a; i >= b; i--)
+#define vi vector<int>
+void solve() ;
 
-int main()
+int32_t main()
 {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    freopen("input.txt",  "r",  stdin);
 #endif
 
-    int T ;
-    cin >> T;
-    vector<int> arr ;
-    for(int i = 0 , n ;i<T ; ++i)
-    {
-        cin>>n;
-        arr.push_back(n);
-    }
-    cout<<unique_number(arr)<<endl;
+    int t = 1;
+	cin >> t;
+    while (t--) solve();
+
     return 0;
+}
+
+void solve()
+{
+    int n ;
+    cin >> n ;
+    if(n>45)
+    {
+        cout << -1 << el ;
+        return ;
+    }
+    string s = "" ;
+    for(int i = 9 ; n ; i--)
+    {
+        if(n<i)
+        {
+            s += to_string(n) ;
+            n = 0 ;
+        }
+        else
+        {
+            s += to_string(i) ;
+            n -= i ;
+        }
+    }
+    reverse(s.begin(), s.end()) ;
+    cout << s << el ;
+    
+    return ;
 }
--- commit.py
+++ commit.py
@@ -1,19 +1,38 @@
-# remove all .exe files from the current repository
 import os
+import glob
+# Remove the all .exe files from the current repository
+# move all .cpp files to ./CPP_files except Rough.cpp
+# add the staged files to the commit and commit the changes with a message
+# push the changes to the remote repository
+
+# / message for the commit
+commit_message = "Automated all final changes to the repository using python script \
+                This is the beautiful script I have ever written for automation"
+
+
+# remove all .exe files from the current repository
 for root, dirs, files in os.walk("."):
     for file in files:
         if (file.endswith(".exe")):
-            os.remove(os.path.join(root, file)) 
+            os.remove(os.path.join(root, file))
+
+print("All .exe files are removed from the current repository\n\n")
+
+# move all .cpp files to ./CPP_files except Rough.cpp
+for file in glob.glob("*.cpp"):
+    if (file != "Rough.cpp"):
+        os.system(f"move {file} ./CPP_files")
+        print(f"Moved {file} to ./CPP_files\n")
+
+print("All .cpp files are moved to ./CPP_files except Rough.cpp\n\n")
+
+# commit and push the changes to the remote repository
 
 # add the staged files to the commit
 os.system("git add .")
-
-#* message for the commit
-s = "Again start to practise Competitive Programming and first time using Python to automate commit"
-
 # commit the changes with a message
-os.system(f"git commit -m \"{s}\"")
+os.system(f"git commit -m \"{commit_message}\"")
 # push the changes to the remote repository
 os.system("git push origin master")
 
-print("All changes are commit and pushed to the remote repository")
+print("All changes are commit and pushed to the remote repository\n\n")
