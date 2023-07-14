ea065afd4fd2b23e7a47adecae797357f5d60767
Debraj-Das <debrajdas.gayatri@gmail.com>
2023-07-14 20:08:32 +0530

    Untrack the remove_exe.py file and modified the Competitive_programming.txt file
    untrack the remove_exe.py file because it is local automation files
    modified the Competitive_programming.txt

--- Rough.cpp
+++ Rough.cpp
@@ -19,7 +19,6 @@ int32_t main()
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #ifndef ONLINE_JUDGE
     freopen("input.txt",  "r",  stdin);
-    freopen("output.txt", "w", stdout);
 #endif
 
     int t = 1;
--- remove_exe.py
+++
@@ -1,8 +0,0 @@
-# remove all .exe files from the current repository
-import os
-for root, dirs, files in os.walk("."):
-    for file in files:
-        if (file.endswith(".exe")):
-            os.remove(os.path.join(root, file)) 
-
-
