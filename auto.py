2dab3f321e0359f7e13b50cdb4818c1b185888e0
Debraj-Das <debrajdas.gayatri@gmail.com>
2023-08-16 11:59:21 +0530

    change auto complete the task

--- auto.py
+++ auto.py
@@ -9,7 +9,7 @@ def move(no_move_files):
     files = [f for f in os.listdir('.') if os.path.isfile(f) if f.endswith(".cpp")]
     for file in files:
         if (file not in no_move_files):
-            os.system(f"move {file} ./Code")
+            os.system(f"move \"{file}\" ./Code")
 
 def commit(commit_message):
     os.system("git add .")
@@ -22,7 +22,7 @@ def main(files, message):
     commit(commit_message=message)
 
 
-not_move_files = {"Rough.cpp", "test.cpp"}
-commit_message = "Review and practise DSA problems"
+not_move_files = {"Rough.cpp", "test.cpp" , "practice_stl.cpp"}
+commit_message = "change auto complete the task"
 
 main(not_move_files, commit_message)
