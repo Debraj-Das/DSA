383aaa369ead30005db1977fc0309d929ca1abcf
Debraj-Das <debrajdas.gayatri@gmail.com>
2023-09-10 10:33:24 +0530

    continue of my competitive journey

---
+++ Code/Buttons.cpp
@@ -0,0 +1,38 @@
+// Problem: A. Buttons
+// URL: https://codeforces.com/problemset/problem/1858/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+
+/*
+    অভিজ্ঞতা একটি কঠিন শিক্ষক,
+    সে প্রথমে তোমার পরীক্ষা নেয় এবং
+    পরে তার পাঠ দেয়।
+*/
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define el '\n'
+#define eif(cds, a, b) ((cds) ? (a) : (b))
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    int __t, a, b, c;
+    cin >> __t;
+    while (__t--)
+    {
+        cin >> a >> b >> c;
+
+        a += (c & 1);
+
+        cout << eif((a > b), "First", "Second") << el;
+    }
+
+    return 0;
+}
