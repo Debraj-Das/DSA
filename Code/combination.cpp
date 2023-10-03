76d43578b061df87f54b52b478b6f0765903c59c
Debraj-Das <debrajdas.gayatri@gmail.com>
2023-10-04 01:29:23 +0530

    Learing the Dynamic programming

---
+++ Code/combination.cpp
@@ -0,0 +1,162 @@
+/*
+    অভিজ্ঞতা একটি কঠিন শিক্ষক,
+    সে প্রথমে তোমার পরীক্ষা নেয় এবং
+    পরে তার পাঠ দেয়।
+*/
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1> void __f(const char *name, Arg1 &&arg1)
+{
+    std::cerr << name << " : " << arg1 << std::endl;
+}
+template <typename Arg1, typename... Args> void __f(const char *names, Arg1 &&arg1, Args &&...args)
+{
+    const char *comma = strchr(names + 1, ',');
+    std::cerr.write(names, comma - names) << " : " << arg1 << '\n';
+    __f(comma + 1, args...);
+}
+
+std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l = 0, int r = 1e9)
+{
+    return std::uniform_int_distribution<int>(l, r)(rng);
+}
+
+#define time(n)                                                                                                        \
+    clock_t tt = clock();                                                                                              \
+    while ((double)(clock() - tt) / CLOCKS_PER_SEC < n)
+
+#define int long long
+#define el '\n'
+#define sp ' '
+
+#define f(i, a, b) for (int i = a; i < (b); i++)
+#define fr(i, a, b) for (int i = a; i >= (b); i--)
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define eif(cds, a, b) ((cds) ? (a) : (b))
+
+#define maxa(a, b) a += ((b)-a) * ((b) > a)
+#define mina(a, b) a -= (a - (b)) * ((b) < a)
+
+#define all(a) a.begin(), a.end()
+#define F(x, a) for (auto x : a)
+
+#define pii pair<int, int>
+#define ff first
+#define ss second
+#define mp make_pair
+
+#define vi vector<int>
+#define pb push_back
+
+template <typename T> using V = vector<T>;
+template <typename T> using pq = priority_queue<T>;
+template <typename T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+template <typename T1, typename T2> using umap = unordered_map<T1, T2>;
+template <typename T> using uset = unordered_set<T>;
+
+inline int exp(int a, int b)
+{
+    int x;
+    for (x = 1; b; a *= a, b >>= 1)
+        x *= (a * (b & 1) + !(b & 1));
+    return x;
+}
+
+/*
+    calculating the combination state;
+
+    // vector<vector<int>> ans;
+    // vector<int> v;
+
+    // for (auto &x : ans)
+    // {
+    // for (auto &y : x)
+    // cout << y << sp;
+    // cout << el;
+    // }
+*/
+
+void combination(int n, int taken, vector<int> &v, V<vi> &ans)
+{
+    if (n == 0)
+    {
+        ans.pb(v);
+        return;
+    }
+
+    for (int i = taken; i <= n; i++)
+    {
+        v.pb(i);
+        combination(n - i, i, v, ans);
+        v.pop_back();
+    }
+
+    return;
+}
+
+inline void solve();
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    int __t;
+    cin >> __t;
+    while (__t--)
+        solve();
+
+    return 0;
+}
+
+int calling;
+V<vi> dp;
+
+int combinationM(int n, int taken)
+{
+    calling++;
+
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    if (n < taken)
+    {
+        return 0;
+    }
+
+    if (dp[n][taken] == -1)
+    {
+        dp[n][taken] = combinationM(n - taken, taken) + combinationM(n, taken + 1);
+    }
+
+    return dp[n][taken];
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    calling = 0;
+
+    dp.resize(n + 1, vi(n + 1, -1));
+
+    int ans = combinationM(n, 1);
+
+    cout << ans << el;
+
+    cerr << calling << sp << ((double)calling / (n * n)) << el;
+
+    return;
+}
