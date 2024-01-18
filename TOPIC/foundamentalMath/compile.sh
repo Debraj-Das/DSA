0b0e33da27a598c3b3c75dc9a1c83e5c03dab218
Debraj-Das <debrajdas.gayatri@gmail.com>
2024-01-18 11:03:30 +0530

    untrack the topic folder

--- TOPIC/foundamentalMath/compile.sh
+++
@@ -1,9 +0,0 @@
-# compile the generator files(cpp) to create gen.exe 
-g++ -Wall -std=c++17 gen.cpp -o gen
-
-echo "gen.cpp complie to gen.exe done"
-
-# compile the expected files(cpp) to create exp.exe
-g++ -Wall -std=c++17 exp.cpp -o exp
-
-echo "exp.cpp complie to exp.exe done"
